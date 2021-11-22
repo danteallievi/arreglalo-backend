e5b411047b18b3317a8da4c6f4339905cd11aaae
Dante Allievi <danteallievi@gmail.com>
2021-11-22 17:40:26 +0100

    Root index file added

--- index.ts
+++ index.ts
@@ -1,34 +1,15 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const debug = require("debug")("Arreglalo:server");
-const chalk = require("chalk");
+require("dotenv").config();
 
-const app = express();
+const { initializeDB } = require("./DB/index");
+const { initializeServer } = require("./server/index");
 
-app.use(cors());
-app.use(express.json());
-app.use(morgan("dev"));
+const PORT = process.env.PORT ?? 5000;
 
-const initializeServer = (port) =>
-  new Promise((resolve, reject) => {
-    const server = app.listen(port, () => {
-      debug(
-        chalk.green(`Listening ${port} port
-http://localhost:${port}`)
-      );
-      resolve(server);
-    });
-    server.on("error", (error) => {
-      debug(chalk.red("There was an error starting the server."));
-      if (error.code === "EADDRINUSE") {
-        debug(chalk.red(`The port ${port} is already in use.`));
-      }
-      reject();
-    });
-    server.on("close", () => {
-      debug(chalk.yellow("Server express disconnected."));
-    });
-  });
-
-module.exports = { initializeServer, app };
+(async () => {
+  try {
+    await initializeDB(process.env.DB_STRING);
+    await initializeServer(PORT);
+  } catch (error) {
+    process.exit(1);
+  }
+})();
