cbf454b1a6ab56c5fa57b6ed1777aa7d0ef6d598
Dante Allievi <danteallievi@gmail.com>
2021-11-22 19:00:01 +0100

    Eslint config changed to fix problems

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,25 @@
 module.exports = {
   env: {
+    browser: true,
     commonjs: true,
     es2021: true,
-    node: true,
     jest: true,
+    node: true,
   },
   extends: ["airbnb-base", "airbnb-typescript/base", "prettier"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
+    project: "./tsconfig.json",
     ecmaVersion: 13,
-    project: tsconfig.json,
   },
+
   plugins: ["@typescript-eslint"],
-  rules: {},
+  rules: {
+    "no-param-reassign": "off",
+    "no-plusplus": "off",
+    "no-restricted-syntax": "off",
+    "consistent-return": "off",
+    "no-debugger": "off",
+    "no-console": "off",
+  },
 };
