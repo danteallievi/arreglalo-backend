dfd4f7a21ba2e30eb96096a44fa6c97e60f52d48
Dante Allievi <danteallievi@gmail.com>
2021-11-25 13:16:37 +0100

    Added method delete on professional route

--- server/routes/professionalRoutes.ts
+++ server/routes/professionalRoutes.ts
@@ -5,6 +5,7 @@ import {
   getProfessional,
   getProfessionalClients,
   hireProfessional,
+  deleteProfessionalProfile,
 } from "../controllers/professionalControllers";
 
 const router = express.Router();
@@ -12,6 +13,7 @@ const router = express.Router();
 router.get("/", getProfessionals);
 router.get("/:id", getProfessional);
 router.post("/hire/:id", checkAuthorization, hireProfessional);
+router.delete("/delete", checkAuthorization, deleteProfessionalProfile);
 router.get("/clients", checkAuthorization, getProfessionalClients);
 
 export default router;
