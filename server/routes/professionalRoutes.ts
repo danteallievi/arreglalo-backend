42be8f0e09dc32c4e8f9691635f4ad10bafeb18d
Dante Allievi <danteallievi@gmail.com>
2021-11-25 12:23:17 +0100

    Route to hire a professional added with POST method

--- server/routes/professionalRoutes.ts
+++ server/routes/professionalRoutes.ts
@@ -4,12 +4,14 @@ import {
   getProfessionals,
   getProfessional,
   getProfessionalClients,
+  hireProfessional,
 } from "../controllers/professionalControllers";
 
 const router = express.Router();
 
 router.get("/", getProfessionals);
 router.get("/:id", getProfessional);
+router.post("/hire/:id", checkAuthorization, hireProfessional);
 router.get("/clients", checkAuthorization, getProfessionalClients);
 
 export default router;
